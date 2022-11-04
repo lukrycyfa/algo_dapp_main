1f034600c62a36ab1076089e3924dde1d8f145ec
lukrycyfa <64338703+lukrycyfa@users.noreply.github.com>
2022-11-04 23:54:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from "react";
-// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.min.css";
-import { createRoot } from "react-dom/client";
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
 
-// createRoot.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+
+createRoot.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
